1a8aa67a2950d7efc67667fa9d8f384aa93d0cfe
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-11-24 23:15:37 -0600

    Add logger and separate responsibilities between main and run

--- scraper.py
+++ scraper.py
@@ -2,33 +2,38 @@
 Get the news for today from 'la republica' and save them into [today] folder as different txt files
 """
 import argparse
+import logging
 
 from typing import List
 
-from parser import XPathParser, NewsParser
+from news_parser import XPathParser, NewsParser
 from store import create_output_folder, save_news_to_folder
 from news import News
 from common import Config
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def run():
+
+def run(config: Config):
     """Get the news for today and save them into [today] folder as different txt files"""
-    # get arguments from yaml
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument('--config_file', help='path to yaml config', default='config.yaml')
-    args = args_parser.parse_args()
+    n_parser: NewsParser = XPathParser()
+    # get news
+    n_parser.parse_home()
+    news: List[News] = n_parser.parse_news()
 
-    # get the configuration from file
-    config = Config(args.config_file)
-    news_parser: NewsParser = XPathParser()
-    # get news links
-    news_parser.parse_home()
-    # get news information
-    news: List[News] = news_parser.parse_news()
     # save news
     output_folder: str = create_output_folder()
     save_news_to_folder(news, output_folder)
 
 
 if __name__ == '__main__':
-    run()
+    # get arguments from yaml
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config_file', help='path to yaml config', default='config.yaml')
+    args = args_parser.parse_args()
+
+    # get the configuration from file
+    cfg = Config(args.config_file)
+    logger.info(f'Beginning scraper for {cfg.sites}')
+    run(cfg)
