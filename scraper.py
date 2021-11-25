ad222344d6e399300a7effd15c19d5aa4d69e045
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-11-24 22:46:38 -0600

    Configure aregument parser

--- scraper.py
+++ scraper.py
@@ -1,16 +1,25 @@
 """
 Get the news for today from 'la republica' and save them into [today] folder as different txt files
 """
+import argparse
 
 from typing import List
 
 from parser import XPathParser, NewsParser
 from store import create_output_folder, save_news_to_folder
 from news import News
+from common import Config
 
 
 def run():
     """Get the news for today and save them into [today] folder as different txt files"""
+    # get arguments from yaml
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config_file', help='path to yaml config', default='config.yaml')
+    args = args_parser.parse_args()
+
+    # get the configuration from file
+    config = Config(args.config_file)
     news_parser: NewsParser = XPathParser()
     # get news links
     news_parser.parse_home()
