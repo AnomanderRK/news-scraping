ad15468e216a92b4b5b8adc9b033c12327d4dce4
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-11-28 23:15:59 -0600

    Add Site class

--- common.py
+++ common.py
@@ -1,7 +1,21 @@
 """Add useful common functions for different parts of the code"""
 
 import yaml
-from typing import List
+from typing import List, Dict, Union
+from dataclasses import dataclass
+
+
+@dataclass
+class Site:
+    """Store data for specific site"""
+    name: str
+    url: str
+    queries: Dict[str, str]
+
+    @property
+    def homepage_links_query(self) -> str:
+        """Get news links from homepage"""
+        return self.queries['homepage_article_links']
 
 
 class Config:
@@ -11,8 +25,18 @@ class Config:
         with open(self.config_path, 'r') as config_file:
             self.config: dict = yaml.load(config_file, yaml.FullLoader)
 
+        # get a list of configs
+        self._sites: Dict[str, Site] = self._get_sites()
+
+    def _get_sites(self) -> Dict[str, Site]:
+        """Get sites from config yaml"""
+        sites: Dict = self.config['news_sites']
+        return {name: Site(name, attrs['url'], attrs['queries']) for name, attrs in sites.items()}
+
     @property
-    def sites(self) -> List[str]:
+    def sites(self) -> Dict[str, Site]:
         """Get sites from configuration"""
-        return list(self.config['news_sites'].keys())
+        return self._sites
+
+
 
