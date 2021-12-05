5e3b59730919adca8613f473ce3f55f108b4ded5
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-12-04 20:55:33 -0600

    Add better docstring to News

--- news.py
+++ news.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 @dataclass
 class News:
-    """Simple structure to store news from a paper"""
+    """Simple structure to store news from a site"""
     title: str
     summary: str
     body: str
