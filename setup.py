5c61b2b142345c934363a7fd1f3b430b1f7b9c03
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-12-08 23:30:22 -0600

    Convert project into module and install it into venv

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        name='news-scraping',
-        version='',
-        packages=['src', 'src.news_scraping', 'tests'],
-        url='',
-        license='',
-        author='AnomanderRK',
-        author_email='',
-        description=''
-    )
+    setup()
