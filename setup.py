613cea3c080ae8be9625ff50e5dd2bc2fdc7ca7d
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-12-08 23:13:39 -0600

    Add initial setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name='news-scraping',
+        version='',
+        packages=['src', 'src.news_scraping', 'tests'],
+        url='',
+        license='',
+        author='AnomanderRK',
+        author_email='',
+        description=''
+    )
