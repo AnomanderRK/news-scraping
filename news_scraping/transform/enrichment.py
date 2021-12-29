e036a4f8ab396f8ebe79d7df4ca48a37b7f2c0e8
AnomanderRK <yaret.mo.kymo@gmail.com>
2021-12-29 10:50:00 -0600

    handle docker error

--- news_scraping/transform/enrichment.py
+++ news_scraping/transform/enrichment.py
@@ -1,7 +1,11 @@
 """Add data to news dataframe"""
 import pandas as pd
+import numpy as np
 from nltk.corpus import stopwords
 import nltk
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Download data needed for nltk analysis
 nltk.download('punkt')
@@ -10,13 +14,18 @@ nltk.download('stopwords')
 SPANISH_STOPWORDS = set(stopwords.words('spanish'))
 
 
-def tokenize_column(news_df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+def tokenize_column(news_df: pd.DataFrame, column_name: str) -> pd.Series:
     """Tokenize column using natural language toolkit"""
-    df: pd.DataFrame = (news_df
+    try:
+        s: pd.Series = (news_df
                         .dropna()
                         .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
                         .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
-                        .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))   # type: ignore
+                        .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))  # type: ignore
                         .apply(lambda word_list: list(filter(lambda word: word not in SPANISH_STOPWORDS, word_list)))
                         .apply(lambda valid_word_list: len(valid_word_list)))
-    return df
+    except Exception as e:
+        logger.error(e)
+        s: pd.Series = pd.Series(len(news_df))
+    print(s)
+    return s
